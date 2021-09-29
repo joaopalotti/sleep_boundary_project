41512db64865f759f715baef10c239e13ea03c98
Joao Palotti <joaopalotti@gmail.com>
2021-09-29 15:21:25 +0300

    Adding "percentage_ground_truth" to features to ignore and removing old results

--- src/ML_pycaret.py
+++ src/ML_pycaret.py
@@ -15,6 +15,7 @@
 
 # +
 import pandas as pd
+
 pd.set_option('display.max_rows', 20)
 
 import numpy as np
@@ -28,24 +29,26 @@ from sklearn.model_selection import KFold
 
 # -
 
-def pycater_setup(train_data, test_data, 
-                  gt_label = "label_5min",
-                  ignore_feat= ["id", "fold", "linetime", "activity", "gt_time"],
+def pycater_setup(train_data, test_data,
+                  gt_label="label_5min",
+                  ignore_feat=["id", "fold", "linetime", "activity", "gt_time"],
                   use_gpu=False):
-    
+    if "percentage_ground_truth" in train_data:
+        ignore_feat.append("percentage_ground_truth")
+
     experiment = setup(data=train_data, test_data=test_data,
                        target=gt_label, session_id=123,
                        normalize=True, transformation=True,
                        fold_strategy="groupkfold", fold_groups="fold",
-                       ignore_features= ignore_feat,
+                       ignore_features=ignore_feat,
                        silent=True, use_gpu=use_gpu,
                        # normalize_method = 'zscore',
-                       normalize_method = 'minmax',       
+                       normalize_method='minmax',
                        # remove_outliers = True,
-                       polynomial_features = True,
+                       polynomial_features=True,
                        # fix_imbalance = True,
                        n_jobs=10
-                   )
+                       )
     return experiment
 
 
@@ -59,12 +62,10 @@ test_data = pd.read_csv(os.path.join(datapath, "test_%s_data.csv.gz" % featset))
 
 experiment = pycater_setup(train_data, test_data, gt_label="ground_truth", ignore_feat=["pid", "fold"])
 
-
 # +
 for m in tqdm(["lr", "rf", "et", "lda", "catboost", "lightgbm"]):
-
     experiment_filename = "sleep_ml_%s_%s_%s" % (m, exp, featset)
-    print("Creating a %s model." % (m))
+    print("Creating a %s model." % m)
     model = create_model(m)
     model = tune_model(model, n_iter=20, choose_better=True)
 
@@ -95,5 +96,3 @@ for m in tqdm(["lr", "rf", "et", "lda", "catboost", "lightgbm"]):
     predictions[["Label", "Score"]].to_csv("%s_predictions.csv.gz" % experiment_filename)
 
     print("Saved TEST results to: %s_predictions.csv.gz" % experiment_filename)
-
-
